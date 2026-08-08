8e9a3865f5998504d3c4b350f642d105649f7fff
Önder Aslan <onderaslan35@gmail.com>
2026-08-08 12:51:23 +0300

    config guncellendi

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // TypeScript hatalarını build aşamasında yok sayar
     ignoreBuildErrors: true,
   },
   eslint: {
-    // ESLint hatalarını build aşamasında yok sayar
     ignoreDuringBuilds: true,
   },
 };
