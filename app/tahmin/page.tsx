fcb78648bf2df1cb9556ac2cce7381a6196cac7c
Önder Aslan <onderaslan35@gmail.com>
2026-08-09 20:31:57 +0300

    Temiz tahminler sayfasi son hali

--- app/tahmin/page.tsx
+++ app/tahmin/page.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useState } from 'react';
-import { useSearchParams } from 'next/navigation';
 
 // 3. Hafta Maç Bülteni
 const matchesData = [
@@ -88,7 +87,6 @@ const usersData = [
 ];
 
 export default function TahminlerPage() {
-  const searchParams = useSearchParams();
   const [selectedUserId, setSelectedUserId] = useState<string>(usersData[0].id);
 
   const selectedUser = usersData.find((u) => u.id === selectedUserId);
