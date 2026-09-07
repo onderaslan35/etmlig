6714b3c1b1dc3d6b27d956c4d5e7988b7c21c47d
Önder Aslan <onderaslan35@gmail.com>
2026-09-07 23:28:22 +0300

    24. mac veritabani kayit sorunu cizgiler sifir yapilarak cozULDU.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -116,7 +116,7 @@ const localTeamLogos: Record<string, string> = {
   "LARNE FC": "https://fr.wikipedia.org/wiki/Special:FilePath/Larne_FC_(logo).svg",
   "KIZILYILDIZ": "https://en.wikipedia.org/wiki/Special:FilePath/Red_Star_Belgrade_crest.svg",
   "LEVADIA FC": "https://en.wikipedia.org/wiki/Special:FilePath/FC_Levadia_Tallinnin.png",
-  "LEVSKI SOFYA": "https://en.wikipedia.org/wiki/Special:FilePath/Levski_Sofia_crest_(2026).svg",
+  "LEVSKI SOFİA": "https://images.fotmob.com/image_resources/logo/teamlogo/8632_large.png",
   "LEVSKI SOFIA": "https://images.fotmob.com/image_resources/logo/teamlogo/8632_large.png",
   "UNIVERSITATEA CRAIOVA": "https://ro.wikipedia.org/wiki/Special:FilePath/CS_Universitatea_Craiova.svg",
   "POLISSYA": "https://en.wikipedia.org/wiki/Special:FilePath/FC_Polissya_Zhytomyr.png",
