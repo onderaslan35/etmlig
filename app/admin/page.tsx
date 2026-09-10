3145070098a509898411871c260942177a59cdb7
Önder Aslan <onderaslan35@gmail.com>
2026-09-10 22:27:47 +0300

    Mac Arsigyhtuhrtvi sayfasina akilli radar eklendi, otomatik hafta degisimi saglandi

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -304,7 +304,7 @@ const localTeamLogos: Record<string, string> = {
   "REIMS": "https://images.fotmob.com/image_resources/logo/teamlogo/9837.png",
 
   // 🔴 HOLLANDA (EREDIVISIE) - FOTMOB
-  "PSV Eindhoven": "https://images.fotmob.com/image_resources/logo/teamlogo/8614.png",
+  "PSV Eindhoven": "https://img.uefa.com/imgml/TP/teams/logos/240x240/50062.png",
   "FEYENOORD": "https://images.fotmob.com/image_resources/logo/teamlogo/8656.png",
   "AZ ALKMAAR": "https://images.fotmob.com/image_resources/logo/teamlogo/8611.png",
 
